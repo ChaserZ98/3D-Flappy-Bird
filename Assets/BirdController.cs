2904bb90fb98fa2e9ae080e640c26b3cc9f12844
ChaserZ98 <feiyuzheng98@gmail.com>
2021-04-05 16:18:23 -0400

    add bird model and BirdController

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -1,68 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening;
-using UnityEngine.UI;
 using UnityEngine;
 
-public class BirdController : MonoBehaviour
-{
-    Animator animator;
-
-    Rigidbody2D rigidbody2d;
-
-    GameController gameController;
-
-    public GameObject look;
-
-    public float speed = 10f;
-
-    public float rowBaiFen = 1f;
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+public class BirdController : MonoBehaviour{
+    protected float jump_speed = 5.0f;
+    public GameObject bird;
+    void Start(){
+        Animation animation = bird.GetComponent<Animation>();
+        animation.Stop();
     }
-
-    private void Update()
-    {
-        look.transform.DORotateQuaternion(new Quaternion(0, 0, rigidbody2d.velocity.y * rowBaiFen, 1), 0.3f);
-        if (!gameController.isGameStart) return;
-        if (Input.GetMouseButtonDown(0))
-        {
-            Fly();
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            this.GetComponent<Rigidbody>().velocity = Vector3.up * this.jump_speed;
+            bird.GetComponent<Animation>().Stop();
+            bird.GetComponent<Animation>().Play();
         }
-    }
-    public void toFly()
-    {
-        rigidbody2d.simulated = true;
-        animator.applyRootMotion = true;
-        animator.SetInteger("State", 1);
-    }
-
-    public void Death()
-    {
-        animator.speed = 0;
-    }
-
-    public void Fly()
-    {
-        if (!gameController.isGameStart) return;
-        rigidbody2d.velocity = new Vector2(0, speed);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (!gameController.isGameStart) return;
-        if (collision.gameObject.tag == "ZhuZi")
-        {
-            Fly();
-            gameController.GameOver();
+        if(Input.GetKey(KeyCode.A)){
+            transform.Translate(Vector3.left * Time.deltaTime * 2);
         }
-        if (collision.gameObject.tag == "PointTrigger")
-        {
-            gameController.score++;
-            gameController.SetScore(gameController.score);
+        else if(Input.GetKey(KeyCode.D)){
+            transform.Translate(Vector3.right * Time.deltaTime * 2);
         }
     }
 }
