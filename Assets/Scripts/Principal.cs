fbb6f07c15f69c54de75b7e5724244c2772abed5
ChaserZ98 <feiyuzheng98@gmail.com>
2021-05-02 05:55:28 -0400

    update

---
+++ Assets/Scripts/Principal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Principal : MonoBehaviour{
+    public GameObject cano;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    }
+}
