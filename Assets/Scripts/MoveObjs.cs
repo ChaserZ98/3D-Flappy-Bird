3659fb33cc7a17e5befca5491b020635a2b58953
ChaserZ98 <feiyuzheng98@gmail.com>
2021-05-02 07:49:03 -0400

    update

---
+++ Assets/Scripts/MoveObjs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjs : MonoBehaviour {
+	// public bool gameStart = false;
+	// Use this for initialization
+	void Start () {
+		// if(gameStart){
+			GetComponent<Rigidbody>().velocity = new Vector3(3f, 0, 0);
+			Invoke("destroi", 16);
+		// }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void destroi()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Principal.cs
+++ Assets/Scripts/Principal.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Principal : MonoBehaviour{
+    public bool gameStart;
+    public bool gameEnd;
     public GameObject cano;
+    // public int fps = 60;
     // Start is called before the first frame update
     void Start(){
-        
+        gameStart = false;
+        InvokeRepeating("CreateObject", 0.0f, 5.0f);
     }
 
     // Update is called once per frame
     void Update(){
 
+    }
+    void GameStart(){
+        gameStart = true;
+    }
+    void GameEnd(){
+        gameEnd = true;
+    }
+    void CreateObject(){
+        if(!gameStart){
+            return;
+        }
+        int i = 1;
+        float z = 0;
+        float x = -30;
+        float y = 0;
+        transform.position = new Vector3(0, 0, z);
+        // GameObject gameObject = new GameObject();
+        switch(i){
+            case 1: GameObject gameObject = Instantiate(cano); gameObject.transform.position = new Vector3(x, y, z); break;
+        }
     }
 }
